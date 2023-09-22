4bdc2cb90c421e315727b3a0376c88769d95d875
phonghao122 <phonghao122@gmail.com>
2023-09-23 00:57:36 +0700

    DbContext

---
+++ Infrastructure/Entities/FashionShopDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//TNT91
+namespace Infrastructure.Entities
+{
+	//TNT91
+	public class FashionShopDbContext : IdentityDbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<BillDetail> BillDetails { get; set; }
+
+		public FashionShopDbContext(DbContextOptions<FashionShopDbContext> options) : base(options)
+		{
+            
+
+        }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+		}
+	}
+}
---
+++ Infrastructure/Entities/FashionShopDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Entities
+{
+	//TNT91
+	public class FashionShopDbContextFactory : IDesignTimeDbContextFactory<FashionShopDbContext>
+    {
+        public FashionShopDbContext CreateDbContext(string[] args)
+        {
+            // chỉ ra sẽ đọc dữ liệu ở trong file appsettings.json của Infrastructure
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<FashionShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new FashionShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
